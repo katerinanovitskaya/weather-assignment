b1d491f52a82d30a7d589ff9085f6993a604b20b
Katerina Novitskaya <k.novitskaya@gmail.com>
2024-04-02 07:58:41 -0700

    Add additional comments for API-based operations and the main function

--- main.js
+++ main.js
@@ -85,35 +85,56 @@ async function getUITemp() {
 //   Integer value of the current temperature for the given city in Fahrenheit
 //
 async function getTempFromAPI() {
+  // Create an instance of the REST client
   var rc = new restClient();
+
+  // Set the address for the geocode API to convert the
+  // city, state, country triplet to latitude and longitude
   var geoCodingRequest = "http://api.openweathermap.org/geo/1.0/direct?q=" +
                          searchCity + "," + searchState + "," + searchCountry +
                          "&limit=1&appid=" + owmAPIKey;
 
+  // Call the geocode API. The API is asynchronous, therefore create a
+  // Promise object and wait for it to return a response.
   let geoCode = await new Promise((resolve, reject) => {
     rc.get(geoCodingRequest, (data, response) => {
+      // Resolve the promise with the data returned by the REST client
       resolve(data);
     });
   });
 
+  // Set the latitude and longitude for the weather API. Use imperial units
+  // to return the temperature in Fahrenheit.
   var weatherRequest = "https://api.openweathermap.org/data/2.5/weather?lat=" +
                        geoCode[0].lat + "&" + "lon=" + geoCode[0].lon +
                        "&units=imperial&appid=" + owmAPIKey;
 
+  // Call the weather API. The API is asynchronous, therefore create a
+  // Promise object and wait for it to return a response.
   let weather = await new Promise((resolve, reject) => {
     rc.get(weatherRequest, (data, response) => {
+      // Resolve the promise with the data returned by the REST client
       resolve(data);
     });
   });
 
+  // Return the temperature
   return weather.main.temp;
 }
 
-// Main function
+// Description:
+//   This is the main function. It invokes the functions to retrieve
+//   the temperature from the UI and API-based sources. It checks for the
+//   variance between to the two and throws a custom exception if the
+//   variance threshold is exceeded, otherwise it prints a message
+//   indicating that the variance is in the accepted range.
 async function main() {
+  // Get temperature via the UI
   let tempFromUI = await getUITemp();
+  // Get temperature via the API
   let tempFromAPI = await getTempFromAPI();
 
+  // Print the results
   console.log("City: " + searchCity + ", " + searchState +
               ", " + searchCountry);
   console.log("Temperature via accuweather.com: " + tempFromUI +
@@ -121,13 +142,18 @@ async function main() {
   console.log("Temperature via openweathermap.org: " + tempFromAPI +
               " deg F");
 
+  // Calculate the variance
   let variance = Math.abs(tempFromUI - tempFromAPI);
+  // Check if the variance is within the threshold
   if (variance > varianceThreshold) {
+    // Variance exceeds threshold, throw an exception
     throw new VarianceError("Temperature variance exceeded by " +
                                 variance + " deg F");
   } else {
+    // Variance within limits
     console.log("Variance within limits");
   }
 }
 
+// Invoke the main function
 main();
